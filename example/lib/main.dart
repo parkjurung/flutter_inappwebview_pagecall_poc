4bce673fe74e883c4c1ba0a6ca71a57dbe184f68
박주렁 <jurung@pplink.net>
2023-01-15 16:36:51 +0900

    sample app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,112 +1,103 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview_pagecall/flutter_inappwebview_pagecall.dart';
 
-import 'package:flutter_inappwebview_example/chrome_safari_browser_example.screen.dart';
-import 'package:flutter_inappwebview_example/headless_in_app_webview.screen.dart';
-import 'package:flutter_inappwebview_example/in_app_webiew_example.screen.dart';
-import 'package:flutter_inappwebview_example/in_app_browser_example.screen.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:permission_handler/permission_handler.dart';
+void main() {
+  runApp(const MyApp());
+}
 
-// InAppLocalhostServer localhostServer = new InAppLocalhostServer(documentRoot: 'assets');
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // await Permission.camera.request();
-  // await Permission.microphone.request();
-  // await Permission.storage.request();
-
-  if (Platform.isAndroid) {
-    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
-
-    var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
-        AndroidWebViewFeature.SERVICE_WORKER_BASIC_USAGE);
-    var swInterceptAvailable = await AndroidWebViewFeature.isFeatureSupported(
-        AndroidWebViewFeature.SERVICE_WORKER_SHOULD_INTERCEPT_REQUEST);
-
-    if (swAvailable && swInterceptAvailable) {
-      AndroidServiceWorkerController serviceWorkerController =
-          AndroidServiceWorkerController.instance();
-
-      await serviceWorkerController
-          .setServiceWorkerClient(AndroidServiceWorkerClient(
-        shouldInterceptRequest: (request) async {
-          print(request);
-          return null;
-        },
-      ));
-    }
+  // This widget is the root of your application....
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page9'),
+    );
   }
-
-  runApp(MyApp());
 }
 
-Drawer myDrawer({required BuildContext context}) {
-  return Drawer(
-    child: ListView(
-      padding: EdgeInsets.zero,
-      children: <Widget>[
-        DrawerHeader(
-          child: Text('flutter_inappbrowser example'),
-          decoration: BoxDecoration(
-            color: Colors.blue,
-          ),
-        ),
-        ListTile(
-          title: Text('InAppBrowser'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/InAppBrowser');
-          },
-        ),
-        ListTile(
-          title: Text('ChromeSafariBrowser'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/ChromeSafariBrowser');
-          },
-        ),
-        ListTile(
-          title: Text('InAppWebView'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/');
-          },
-        ),
-        ListTile(
-          title: Text('HeadlessInAppWebView'),
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
-          },
-        ),
-      ],
-    ),
-  );
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
+class _MyHomePageState extends State<MyHomePage> {
 
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
+  void _openNewWindow() async {
+    // navigator.push
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('New Window'),
+        ),
+        body: Column(children: [
+          Expanded(
+            child: InAppWebView(
+              // ....
+              key: UniqueKey(),
+              initialUrlRequest: URLRequest(url: Uri.parse('https://demo.pagecall.net/join/dajagan/221111abc')),
+              initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                    mediaPlaybackRequiresUserGesture: false,
+                    javaScriptEnabled: true),
+                ios: IOSInAppWebViewOptions(
+                    allowsInlineMediaPlayback: true,
+                    allowsAirPlayForMediaPlayback: true),
+              )
 
-  @override
-  void dispose() {
-    super.dispose();
+      ))]));
+    }));
+    print('open');
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(initialRoute: '/', routes: {
-      '/': (context) => InAppWebViewExampleScreen(),
-      '/InAppBrowser': (context) => InAppBrowserExampleScreen(),
-      '/ChromeSafariBrowser': (context) => ChromeSafariBrowserExampleScreen(),
-      '/HeadlessInAppWebView': (context) => HeadlessInAppWebViewExampleScreen(),
-    });
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _openNewWindow,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
